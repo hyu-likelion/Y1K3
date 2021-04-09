eccaae135c89e708d07f15702aa16f1d2348ac4f
justin1228 <didls1228@hanyang.ac.kr>
2021-04-09 20:10:27 +0900

    calculator

--- week2/hyungeun/Calculator/calculator/views.py
+++ week2/hyungeun/Calculator/calculator/views.py
@@ -10,6 +10,6 @@ def main(request):
 def calculator(request):
     expression = request.GET['expression']
     result = eval(expression)
-    return render(request, "calculator/calculator.html", {"result": result})
+    return render(request, "calculator/calculator.html", {"result":result})
     # GET으로 값을 받고, eval 함수를 사용해 값 계산
     # calculator 페이지 렌더링
