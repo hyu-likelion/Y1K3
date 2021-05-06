54f204897d87251cb90b67f3494b240acfc935c4
justin1228 <didls1228@hanyang.ac.kr>
2021-05-06 21:04:17 +0900

    authentication

--- week3/hyungeun/CRUD/lionproject/account/models.py
+++ week3/hyungeun/CRUD/lionproject/account/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    nickname = models.CharField(max_length=15)
+    university = models.CharField(max_length=50)
+    location = models.CharField(max_length=200)
